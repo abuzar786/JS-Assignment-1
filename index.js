572f50f631e997e0969cc09a1a25b7cf53593406
abuzar786 <49241793+abuzar786@users.noreply.github.com>
2022-10-15 14:51:03 +0200

    finished assignment

---
+++ index.js
@@ -0,0 +1,81 @@
+/* 
+Main JS Assignment
+*/
+
+/* Initialize the Computer and Player Score and Message */
+const computerSelectionArray = ["Rock", "Paper", "Scissors"];
+totalRounds = 5;
+let playerSore = 0;
+let computerScore = 0;
+let result = "";
+
+/*
+ Computer function where computer choosing a random option from array.
+ */
+function computerPlay() {
+  let randomComputerChoice = Math.floor(
+    Math.random() * computerSelectionArray.length
+  );
+  return computerSelectionArray[randomComputerChoice];
+}
+
+/*
+ 
+ */
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return (result = "Draw");
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return (result = `Player Won! ${playerSelection} beats ${computerSelection}`);
+  } else if (playerSelection === "paper" && computerSelection === "rock")
+    return (result = `Player Won! ${playerSelection} beats ${computerSelection}`);
+  else if (playerSelection === "scissors" && computerSelection === "paper")
+    return (result = `Player Won! ${playerSelection} beats ${computerSelection}`);
+  else
+    return (result = `Computer Won! ${computerSelection} beats ${playerSelection}`);
+}
+
+/*
+game function is a loop function and it runs 5 times
+  */
+function game() {
+  for (let i = 0; i < totalRounds; i++) {
+    let computerSelection = computerPlay();
+    let playerSelection = prompt(
+      "Pleae select ande write your input: Rock, Paper, or Scissors."
+    );
+    playRound(playerSelection, computerSelection);
+    console.log(result);
+
+    if (result.includes("Player Won")) {
+      playerSore++;
+      console.log(
+        `Player score: ${playerSore} Computer score: ${computerScore}`
+      );
+    } else if (result.includes("Computer Won")) {
+      computerScore++;
+      console.log(
+        `Player score: ${playerSore} Computer score: ${computerScore}`
+      );
+    } else {
+      console.log(
+        `Player score: ${playerSore} Computer score: ${computerScore}`
+      );
+    }
+  }
+  /*
+   */
+
+  playerSore > computerScore
+    ? console.log(
+        `Player win the game! Player score: ${playerSore} Computer score: ${computerScore}`
+      )
+    : console.log(
+        `Computer win the game! Player score: ${playerSore} Computer score: ${computerScore}`
+      );
+  return result;
+}
+game();
