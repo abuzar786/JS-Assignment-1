7c3e0500ded0fe956c7aa10cdf81a1e3bf21aa5f
abuzar786 <49241793+abuzar786@users.noreply.github.com>
2022-10-15 15:14:37 +0200

    more update

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ function game() {
     } else {
       i--;
       console.log(`Invalid input`);
+      continue;
     }
     let computerSelection = computerPlay();
     playRound(playerSelection, computerSelection);
